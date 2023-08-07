22ea96ea857c9895d43b0f6686c213d2303b5b74
shiftysloth <no>
2023-08-07 23:29:55 +0200

    feat: page skeleton with basic wallet check

---
+++ pages/reveal/index.tsx
@@ -0,0 +1,42 @@
+import { useAuthContext } from "../../hooks/useAuthContext";
+
+const Apelleons = () => {
+  const { wallet } = useAuthContext();
+  console.log(wallet);
+
+  if (!wallet || wallet?.type !== "stargaze") {
+    return (
+      <section className="m-10 mt-4">
+        <div className="w-full flex flex-col items-center">
+          <h1 className="text-center font-bold text-3xl mb-2">NFT Reveal</h1>
+          <p className="text-center text-2xl mt-4 mb-8">
+            Connect your Stargaze wallet
+          </p>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="m-10 mt-4">
+      <div className="w-full flex flex-col items-center">
+        <h1 className="text-center font-bold text-3xl mb-2">NFT Reveal</h1>
+        <select
+          className=" border text-md rounded-lg block w-full p-2.5 bg-gray-700 border-gray-500  text-white max-w-md"
+          defaultValue="DEFAULT"
+        >
+          <option value="DEFAULT" disabled>
+            Choose a collection
+          </option>
+          <option key="apelleons" value="apelleons">
+            Apelleons
+          </option>
+        </select>
+
+        <pre className="text-center text-2xl mt-4 mb-8">work in progress</pre>
+      </div>
+    </section>
+  );
+};
+
+export default Apelleons;
