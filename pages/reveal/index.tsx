5fb70236c428f6dba29bce1fe95966d6664a16e2
shiftysloth <no>
2023-08-08 23:22:47 +0200

    feat: select logic

--- pages/reveal/index.tsx
+++ pages/reveal/index.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 import NFTHoverCard from "../../components/NFTHoverCard";
 import { useAuthContext } from "../../hooks/useAuthContext";
 
+enum Collection {
+  DEFAULT = "DEFAULT",
+  APELLEONS = "apelleons",
+}
+
 const Apelleons = () => {
   const { wallet } = useAuthContext();
+  const [selectedCollection, setSelectedCollection] = useState<Collection>(
+    Collection.DEFAULT
+  );
 
   if (!wallet || wallet?.type !== "stargaze") {
     return (
@@ -23,25 +33,31 @@ const Apelleons = () => {
         <h1 className="text-center font-bold text-3xl mb-2">NFT Reveal</h1>
         <select
           className=" border text-md rounded-lg block w-full p-2.5 bg-gray-700 border-gray-500  text-white max-w-md"
-          defaultValue="DEFAULT"
+          value={selectedCollection}
+          onChange={(e) => {
+            setSelectedCollection(e.target.value as Collection);
+          }}
         >
-          <option value="DEFAULT" disabled>
+          <option value={Collection.DEFAULT} disabled>
             Choose a collection
           </option>
-          <option key="apelleons" value="apelleons">
+          <option key={Collection.APELLEONS} value={Collection.APELLEONS}>
             Apelleons
           </option>
         </select>
 
         <pre className="text-center text-2xl mt-4 mb-8">work in progress</pre>
-        <div className="flex flex-wrap justify-center gap-4">
-          <NFTHoverCard nftId={5} />
-          <NFTHoverCard nftId={57} />
-          <NFTHoverCard nftId={114} />
-          <NFTHoverCard nftId={118} />
-          <NFTHoverCard nftId={1425} />
-          <NFTHoverCard nftId={3452} />
-        </div>
+
+        {selectedCollection === Collection.APELLEONS && (
+          <div className="flex flex-wrap justify-center gap-4">
+            <NFTHoverCard nftId={5} />
+            <NFTHoverCard nftId={57} />
+            <NFTHoverCard nftId={114} />
+            <NFTHoverCard nftId={118} />
+            <NFTHoverCard nftId={1425} />
+            <NFTHoverCard nftId={3452} />
+          </div>
+        )}
       </div>
     </section>
   );
